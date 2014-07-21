676d82d96bc8bf1958776ea7822affed59b87115
James Simpson <james33@gmail.com>
2014-07-21 12:20:37 -0500

    v1.1.5

--- CanvasInput.js
+++ CanvasInput.js
@@ -1,5 +1,5 @@
 /*!
- *  CanvasInput v1.1.4
+ *  CanvasInput v1.1.5
  *  http://goldfirestudios.com/blog/108/CanvasInput-HTML5-Canvas-Text-Input
  *
  *  (c) 2013-2014, James Simpson of GoldFire Studios
